ebc10cf72a4cc4debe81544bcfec89b9764b40f1
Reese Mothershed <reesemothershed@gmail.com>
2026-09-22 15:33:02 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Capstone_UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuExitButton(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Play_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new GameSelection());
+        }
+    }
+}
